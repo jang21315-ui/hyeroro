c8f04d6f75db0c982d47433210d149af00ba6b07
영진 <jang21315@gmail.com>
2026-09-16 20:03:24 +0900

    Google 로그인 콜백 주소 수정

--- components/GoogleLoginButton.tsx
+++ components/GoogleLoginButton.tsx
@@ -9,7 +9,7 @@ export default function GoogleLoginButton() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/auth`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
 
